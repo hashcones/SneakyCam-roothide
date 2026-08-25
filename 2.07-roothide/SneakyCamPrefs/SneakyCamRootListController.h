d1e3e8fe2d0a7898ecc9b585469c26e3c966fdd4
hashcones <jimij99@hotmail.com>
2026-08-25 04:39:03 +0000

    Packed new rootless deb, just need to roothide-patch it.

--- 2.07-roothide/SneakyCamPrefs/SneakyCamRootListController.h
+++
@@ -1,7 +0,0 @@
-#import <Preferences/PSListController.h>
-#import <Preferences/PSSpecifier.h>
-
-@interface SneakyCamRootListController : PSListController
-- (void)respring;
-- (void)clearLogFile;
-@end
